f6978b6659b08b22303dc7b6ae81f1559ff7d99b
greatby <gills_halogen2i@icloud.com>
2025-10-29 14:24:55 +0530

    Minor code changes

--- src/app/robots.js
+++ src/app/robots.js
@@ -2,6 +2,6 @@
 export default function robots() {
  return {
  rules: [{ userAgent: '*', allow: '/' }],
- sitemap: 'https://workforce.com/sitemap.xml',
+ sitemap: 'https://wflimited.com/sitemap.xml',
  }
 }
